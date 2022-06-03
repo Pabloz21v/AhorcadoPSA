7c26af27eebd9320b19f33fe1f0bb08106cd4045
Pabloz21v <pabloz21v@gmail.com>
2022-06-03 18:44:54 -0300

    boton

---
+++ script/inicio.js
@@ -0,0 +1,9 @@
+let botonInicio = document.querySelector("#botonInicio");
+
+botonInicio.addEventListener("click",function(event) {
+    event.preventDefault();
+    
+  <a href="AhorcadoPSA/ahorcado_play.html"></a>
+  
+})
+
