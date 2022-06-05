688f8053228d85241698f1b27cd8602b2d3f34a9
Pabloz21v <pabloz21v@gmail.com>
2022-06-04 22:25:56 -0300

    4-6

--- script/ahorcado_play.js
+++ script/ahorcado_play.js
@@ -4,6 +4,7 @@ let seisLetras=["cantar","cabeza","cursos","oracle","discos","diosas","doctor","
 let sieteLetras=["ciervos","experto","experta","idiomas","grafico","parques","plantas","siervos"];
 let ochoLetras=["batallas","deportes","clientes","espacios","ejercito","juguetes","pajarito"];
 let arrayPalabras=[cuatroLetras, cincoLetras, seisLetras, sieteLetras, ochoLetras];
+let arrayLetrasErradas = [];
 
 //eleccion de palabra----------------
 let elegirLargoDePalabra = Math.round(Math.random()*(arrayPalabras.length-1));
@@ -24,29 +25,57 @@ for (let i = 0; i < largoDeLaPalabra; i++) {
 }
 letrasCorrectas.appendChild(letra)
 
-//al acertar letras------------------------------------------
-
-
+//al precionar una tecla ---------------------------------
 const body = document.querySelector("body")
+
 body.addEventListener("keydown",function(event) {
     event.preventDefault();
     
     let letraPrecionada = event.key
     for (let i = 0; i < largoDeLaPalabra; i++) {
+        
         if (letraPrecionada == palabraElegida[i]) {
-            idcorrecto = "#"+palabraElegida[i]+[i] +""
-        let ids = document.querySelector(idcorrecto)
-             console.log(idcorrecto)
-             ids.classList.add("visible")
-             ids.classList.remove("invisible")
-        } else {
-
-        }
+            idCorrecto = "#"+palabraElegida[i]+[i] 
+            let ids = document.querySelector(idCorrecto)
+            ids.classList.add("visible")
+            ids.classList.remove("invisible")
+        }   
+            
     }
+    console.log(palabraElegida)
+    let arrayDePalabra = palabraElegida.split("");
 
-
+    function existira(letraPrecionada) {
+        let existe = arrayDePalabra.indexOf(letraPrecionada);  
+       
+        if (existe == -1) {
+            const letrasIncorrectas = document.querySelector("#letrasIncorrectas")
+            const letraIncorrecta =  document.createDocumentFragment()
+            const crearLi = document.createElement("LI")
+            crearLi.innerHTML = "<p class='letraErrada'> " + letraPrecionada + "</p>"
+            letraIncorrecta.appendChild(crearLi)
+            letrasIncorrectas.appendChild(letraIncorrecta)
+        } 
+    }
+    existira(letraPrecionada)
+    grabarLetras(letraPrecionada)
 })
-console.log(palabraElegida)
+
+for (let i = 0; i < largoDeLaPalabra; i++) {
+    arrayLetrasErradas.push(palabraElegida[i])
+}
+
+
+function grabarLetras(letraPrecionada) {
+    if (arrayLetrasErradas.indexOf(letraPrecionada) == -1) {
+        arrayLetrasErradas.push(letraPrecionada)
+        
+    }
+    
+    console.log(arrayLetrasErradas)
+    
+}
+
 
 //creacion de renglones acorde a la palabra--------------------
 const renglones = document.querySelector("#renglones")
